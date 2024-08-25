c7b7083443c23a7ca0774e62859a239ea98e4f9e
Khelifa Saadallah Oussama <oussakhema00@gmail.com>
2024-08-25 16:21:50 +0200

    test api route

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,5 @@
+
+export const testUser = (req, res, next) => {
+    res.send('API is running...');
+    next();
+}
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import connectDB from './config/db';
+import connectDB from './config/db.js';
 import dotenv from 'dotenv';
-
+import userRoute from './routes/user.route.js';
 dotenv.config();
 
 const app = express();
@@ -11,6 +11,7 @@ app.use(express.json());
 connectDB();
 
 
+app.use("/api/user", userRoute)
 
 
 
