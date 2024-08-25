51298280d0afb97d3b0f179cbc9312961df8a6b8
Khelifa Saadallah Oussama <oussakhema00@gmail.com>
2024-08-25 14:05:45 +0200

    First push

---
+++ server/Module/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: mongoose.Schema.Types.String,
+        required: true,
+    },
+
+    username: {
+        type: mongoose.Schema.Types.String,
+        required: true,
+
+    },
+
+    email: {
+        type: mongoose.Schema.Types.String,
+        required: true,
+        unique: true,
+        password: {
+            type: mongoose.Schema.Types.String,
+            required: true,
+        },
+    }
+},
+    { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import connectDB from './config/db';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+
+
+connectDB();
+
+
+
+
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
