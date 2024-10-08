534f953f84b46d01c80063f2fd447412520ff454
Khelifa Saadallah Oussama <oussakhema00@gmail.com>
2024-10-09 01:53:39 +0200

    ...

--- server/index.js
+++ server/index.js
@@ -5,15 +5,25 @@ import userRoute from './routes/user.route.js';
 import authRoute from './routes/auth.route.js';
 import passport from 'passport';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
+const __dirname = path.resolve();
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 app.use(express.json());
 
 connectDB();
 
 app.use(cookieParser());
+
 
 
 app.use("/api/users", userRoute)
